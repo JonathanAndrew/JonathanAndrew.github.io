2b580057d512ebabeec949ab5eb6f0219f084c42
JonathanAndrew <jonandrewstevenson@yahoo.com>
2016-04-26 16:57:22 -0700

    styling updates

--- js/index.js
+++ js/index.js
@@ -1,20 +1,49 @@
 $(document).ready(function(){
-
- $('#about-link').click(function(event){
- 	event.preventDefault();
- 	$("#home").addClass('hide');
- 	$("#about-link").addClass('hide');
- 	$("#about").removeClass('hide');
- 	$("#portfolio-link").removeClass('hide');
-
- });
-
- $('#portfolio-link').click(function(event){
- 	event.preventDefault(event);
- 	$('#about').addClass('hide');
- 	$('#home').removeClass('hide');
- 	$('#portfolio-link').addClass('hide');
- 	$('#about-link').removeClass('hide');
- });
+	$('.brand-logo').click(function(){
+		window.location.replace("")
+	});
+ 	$('#about-link').click(function(event){
+	 	event.preventDefault();
+	 	window.scrollTo(0,0);
+	 	$("#home").addClass('hide');
+	 	$("#about-link").addClass('hide');
+	 	$("#about").removeClass('hide');
+	 	$("#portfolio-link").removeClass('hide');
+ 	});
+ 	 
+ 	$('.about-link').click(function(event){
+	 	event.preventDefault();
+	 	window.scrollTo(0,0);
+	 	$("#home").addClass('hide');
+	 	$("#about-link").addClass('hide');
+	 	$("#about").removeClass('hide');
+	 	$("#portfolio-link").removeClass('hide');
+	 	$(function() {
+	        setTimeout(function() {
+	          $("#sidenav-overlay").trigger("click");
+	        }, 100);
+      	});
+ 	});
+ 	$('#portfolio-link').click(function(event){
+	 	event.preventDefault(event);
+	 	window.scrollTo(0,0);
+	 	$('#about').addClass('hide');
+	 	$('#home').removeClass('hide');
+	 	$('#portfolio-link').addClass('hide');
+	 	$('#about-link').removeClass('hide');
+ 	});
+ 	$('.portfolio-link').click(function(event){
+	 	event.preventDefault(event);
+	 	window.scrollTo(0,0);
+	 	$('#about').addClass('hide');
+	 	$('#home').removeClass('hide');
+	 	$('#portfolio-link').addClass('hide');
+	 	$('#about-link').removeClass('hide');
+	 	$(function() {
+	        setTimeout(function() {
+	          $("#sidenav-overlay").trigger("click");
+	        }, 100);
+      	});
+ 	});
 
 });
