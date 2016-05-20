214c95174aa9b2a0c841e779f3dd9b1d62ddcd01
JonathanAndrew <jonandrewstevenson@yahoo.com>
2016-05-20 12:04:28 -0700

    updates

--- js/index.js
+++ js/index.js
@@ -2,11 +2,20 @@ $(document).ready(function(){
 	$('.brand-logo').click(function(){
 		window.location.replace("")
 	});
+	// $('.slider').slider({full_width: true});
+	// $('.slider').slider('pause');
+	// // Start slider
+	// $('.slider').slider('start');
+	// // Next slide
+	// $('.slider').slider('next');
+	// // Previous slide
+	// $('.slider').slider('prev');
  	$('#about-link').click(function(event){
 	 	event.preventDefault();
 	 	window.scrollTo(0,0);
 	 	$("#home").addClass('hide');
 	 	$("#about-link").addClass('hide');
+	 	$("#photography").addClass('hide');
 	 	$("#about").removeClass('hide');
 	 	$("#portfolio-link").removeClass('hide');
  	});
@@ -16,7 +25,9 @@ $(document).ready(function(){
 	 	window.scrollTo(0,0);
 	 	$("#home").addClass('hide');
 	 	$("#about-link").addClass('hide');
+	 	$("#photography").addClass('hide');
 	 	$("#about").removeClass('hide');
+	 	$("#photography-link").removeClass('hide');
 	 	$("#portfolio-link").removeClass('hide');
 	 	$(function() {
 	        setTimeout(function() {
@@ -29,6 +40,8 @@ $(document).ready(function(){
 	 	window.scrollTo(0,0);
 	 	$('#about').addClass('hide');
 	 	$('#home').removeClass('hide');
+	 	$("#photography").addClass('hide');
+	 	$("#photography-link").removeClass('hide');
 	 	$('#portfolio-link').addClass('hide');
 	 	$('#about-link').removeClass('hide');
  	});
@@ -37,6 +50,7 @@ $(document).ready(function(){
 	 	window.scrollTo(0,0);
 	 	$('#about').addClass('hide');
 	 	$('#home').removeClass('hide');
+	 	$("#photography").addClass('hide');
 	 	$('#portfolio-link').addClass('hide');
 	 	$('#about-link').removeClass('hide');
 	 	$(function() {
@@ -45,5 +59,29 @@ $(document).ready(function(){
 	        }, 100);
       	});
  	});
+ 	// $('#photography-link').click(function(event){
+	 // 	event.preventDefault(event);
+	 // 	window.scrollTo(0,0);
+	 // 	$('#about').addClass('hide');
+	 // 	$('#home').addClass('hide');
+	 // 	$("#photography").removeClass('hide');
+	 // 	$("#photography-link").addClass('hide');
+	 // 	$('#portfolio-link').removeClass('hide');
+	 // 	$('#about-link').removeClass('hide');
+ 	// });
+ 	// $('.photography-link').click(function(event){
+	 // 	event.preventDefault(event);
+	 // 	window.scrollTo(0,0);
+	 // 	$('#about').addClass('hide');
+	 // 	$('#home').addClass('hide');
+	 // 	$("#photography").removeClass('hide');
+	 // 	$('#portfolio-link').addClass('hide');
+	 // 	$('#about-link').removeClass('hide');
+	 // 	$(function() {
+	 //        setTimeout(function() {
+	 //          $("#sidenav-overlay").trigger("click");
+	 //        }, 100);
+  //     	});
+ 	// });
 
 });
